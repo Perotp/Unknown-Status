61c03a56b43a26670c9d320815ccb10cb1dbb33d
Perotp <paul.perot@thyssenkrupp.com>
2024-07-19 09:27:36 -0400

    frida

---
+++ pyautogui rgb color.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+iml = pyautogui.screenshot(region=(200,200,200,50))
+iml.save(r'C:\\Users\\PWPI9\\Perot Solutions\\OneDrive - Perot Solutions\\Documents\\GitHub\\Unknown-Status\\savedimage.png')
